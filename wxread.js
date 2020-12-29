4cd7afbfe20e4f56de733c155a458a56ea03c7a2
LiXueLong <li_xuelong@126.com>
2020-12-29 10:12:14 +0800

    Update wxread.js

--- wxread.js
+++ wxread.js
@@ -1,110 +1,38 @@
-function Base64() {
- 
-	// private property
-	_keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
- 
-	// public method for encoding
-	this.encode = function (input) {
-		var output = "";
-		var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-		var i = 0;
-		input = _utf8_encode(input);
-		while (i < input.length) {
-			chr1 = input.charCodeAt(i++);
-			chr2 = input.charCodeAt(i++);
-			chr3 = input.charCodeAt(i++);
-			enc1 = chr1 >> 2;
-			enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-			enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-			enc4 = chr3 & 63;
-			if (isNaN(chr2)) {
-				enc3 = enc4 = 64;
-			} else if (isNaN(chr3)) {
-				enc4 = 64;
-			}
-			output = output +
-			_keyStr.charAt(enc1) + _keyStr.charAt(enc2) +
-			_keyStr.charAt(enc3) + _keyStr.charAt(enc4);
-		}
-		return output;
-	}
- 
-	// public method for decoding
-	this.decode = function (input) {
-		var output = "";
-		var chr1, chr2, chr3;
-		var enc1, enc2, enc3, enc4;
-		var i = 0;
-		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-		while (i < input.length) {
-			enc1 = _keyStr.indexOf(input.charAt(i++));
-			enc2 = _keyStr.indexOf(input.charAt(i++));
-			enc3 = _keyStr.indexOf(input.charAt(i++));
-			enc4 = _keyStr.indexOf(input.charAt(i++));
-			chr1 = (enc1 << 2) | (enc2 >> 4);
-			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-			chr3 = ((enc3 & 3) << 6) | enc4;
-			output = output + String.fromCharCode(chr1);
-			if (enc3 != 64) {
-				output = output + String.fromCharCode(chr2);
-			}
-			if (enc4 != 64) {
-				output = output + String.fromCharCode(chr3);
-			}
-		}
-		output = _utf8_decode(output);
-		return output;
-	}
- 
-	// private method for UTF-8 encoding
-	_utf8_encode = function (string) {
-		string = string.replace(/\r\n/g,"\n");
-		var utftext = "";
-		for (var n = 0; n < string.length; n++) {
-			var c = string.charCodeAt(n);
-			if (c < 128) {
-				utftext += String.fromCharCode(c);
-			} else if((c > 127) && (c < 2048)) {
-				utftext += String.fromCharCode((c >> 6) | 192);
-				utftext += String.fromCharCode((c & 63) | 128);
-			} else {
-				utftext += String.fromCharCode((c >> 12) | 224);
-				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
- 
-		}
-		return utftext;
-	}
- 
-	// private method for UTF-8 decoding
-	_utf8_decode = function (utftext) {
-		var string = "";
-		var i = 0;
-		var c = c1 = c2 = 0;
-		while ( i < utftext.length ) {
-			c = utftext.charCodeAt(i);
-			if (c < 128) {
-				string += String.fromCharCode(c);
-				i++;
-			} else if((c > 191) && (c < 224)) {
-				c2 = utftext.charCodeAt(i+1);
-				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-				i += 2;
-			} else {
-				c2 = utftext.charCodeAt(i+1);
-				c3 = utftext.charCodeAt(i+2);
-				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-				i += 3;
-			}
-		}
-		return string;
-	}
+//let wxreadData = 'ewogICJzdGFydFRpbWUiOiA5OTE2MDM5NDAxMzIsCiAgImV4cGlyZWRUaW1lIjogOTkxNjA0MjQ2Mzk5LAogICJleHBpcmVkIjogMCwKICAiaXNQYXlpbmciOiAwLAogICJwZXJtYW5lbnQiOiAwLAogICJkYXkiOiA5MSwKICAicmVtYWluVGltZSI6IDk5MzA2MTk2LAogICJwYXlpbmdSZW1haW5UaW1lIjogNiwKICAiaXNBdXRvUmVuZXdhYmxlIjogNiwKICAiaGlzdG9yeUF1dG9SZW5ld2FibGUiOiA2LAogICJhdXRvUmVuZXdhYmxlQ2hhbm5lbCI6IDYsCiAgImF1dG9SZW5ld2FibGVUaW1lIjogNiwKICAiYXV0b1JlbmV3YWJsZVByaWNlIjogOTkxOTAwLAogICJzYXZlZE1vbmV5IjogOTkyNzQzMywKICAidG90YWxGcmVlUmVhZERheSI6IDY2NjYsCiAgInJlbWFpbkNvdXBvbiI6IDY2NjYsCiAgInJlbWFpbkNvdW50IjogNjY2NiwKICAiaGludHNGb3JSZWNoYXJnZSI6IHsKICAgICJwcmVkaWN0ZWRTYXZlZE1vbmV5IjogOTkxMDMxNSwKICAgICJwcmVkaWN0ZWRDaGFwdGVyUHJpY2UiOiAxNSwKICAgICJwcmljZVBlck1vbnRoIjogOTk5MDAsCiAgICAic2VuZENvdXBvbnMiOiA2NjY2LAogICAgImJ1dHRvblRpdGxlIjogIuS7mOi0ueaXoOmZkOWNoTnlhYMv5pyIIMK3IOmZkOaXtueJueaDoCIsCiAgICAiYnV0dG9uU3VidGl0bGUiOiAi5L2g5q2j5Zyo5L2/55So5YWN6LS55peg6ZmQ5Y2hIMK3IOWNh+e6p+S7mOi0ueaXoOmZkOWNoeWNs+WPr+mYheivuyIKICB9LAogICJmcmVlQm9va0lkcyI6IFtdLAogICJ0aW1lc3RhbXAiOiAxNjAzOTQwMjAzLAogICJyYW5kb20iOiA4MzQxLAogICJzaWduYXR1cmUiOiAiNWQ3ZjZjOTI5MDg2YTRhOTU4ZWFkNjBjOWJkOTVmYWU2OTkxYWZlZjUxYzFkZTUwYTQxZTdjMjY0MWZkYWYzZiIKfQ==';
+//wxreadData = new Base64().decode(wxreadData)
+
+let wxreadData = {
+  "startTime": 991603940132,
+  "expiredTime": 991604246399,
+  "expired": 0,
+  "isPaying": 0,
+  "permanent": 0,
+  "day": 91,
+  "remainTime": 99306196,
+  "payingRemainTime": 6,
+  "isAutoRenewable": 6,
+  "historyAutoRenewable": 6,
+  "autoRenewableChannel": 6,
+  "autoRenewableTime": 6,
+  "autoRenewablePrice": 991900,
+  "savedMoney": 9927433,
+  "totalFreeReadDay": 6666,
+  "remainCoupon": 6666,
+  "remainCount": 6666,
+  "hintsForRecharge": {
+    "predictedSavedMoney": 9910315,
+    "predictedChapterPrice": 15,
+    "pricePerMonth": 99900,
+    "sendCoupons": 6666,
+    "buttonTitle": "付费无限卡9元/月 · 限时特惠",
+    "buttonSubtitle": "你正在使用免费无限卡 · 升级付费无限卡即可阅读"
+  },
+  "freeBookIds": [],
+  "timestamp": 1603940203,
+  "random": 8341,
+  "signature": "5d7f6c929086a4a958ead60c9bd95fae6991afef51c1de50a41e7c2641fdaf3f"
 }
-
-
-let wxreadData = 'ewogICJzdGFydFRpbWUiOiA5OTE2MDM5NDAxMzIsCiAgImV4cGlyZWRUaW1lIjogOTkxNjA0MjQ2Mzk5LAogICJleHBpcmVkIjogMCwKICAiaXNQYXlpbmciOiAwLAogICJwZXJtYW5lbnQiOiAwLAogICJkYXkiOiA5MSwKICAicmVtYWluVGltZSI6IDk5MzA2MTk2LAogICJwYXlpbmdSZW1haW5UaW1lIjogNiwKICAiaXNBdXRvUmVuZXdhYmxlIjogNiwKICAiaGlzdG9yeUF1dG9SZW5ld2FibGUiOiA2LAogICJhdXRvUmVuZXdhYmxlQ2hhbm5lbCI6IDYsCiAgImF1dG9SZW5ld2FibGVUaW1lIjogNiwKICAiYXV0b1JlbmV3YWJsZVByaWNlIjogOTkxOTAwLAogICJzYXZlZE1vbmV5IjogOTkyNzQzMywKICAidG90YWxGcmVlUmVhZERheSI6IDY2NjYsCiAgInJlbWFpbkNvdXBvbiI6IDY2NjYsCiAgInJlbWFpbkNvdW50IjogNjY2NiwKICAiaGludHNGb3JSZWNoYXJnZSI6IHsKICAgICJwcmVkaWN0ZWRTYXZlZE1vbmV5IjogOTkxMDMxNSwKICAgICJwcmVkaWN0ZWRDaGFwdGVyUHJpY2UiOiAxNSwKICAgICJwcmljZVBlck1vbnRoIjogOTk5MDAsCiAgICAic2VuZENvdXBvbnMiOiA2NjY2LAogICAgImJ1dHRvblRpdGxlIjogIuS7mOi0ueaXoOmZkOWNoTnlhYMv5pyIIMK3IOmZkOaXtueJueaDoCIsCiAgICAiYnV0dG9uU3VidGl0bGUiOiAi5L2g5q2j5Zyo5L2/55So5YWN6LS55peg6ZmQ5Y2hIMK3IOWNh+e6p+S7mOi0ueaXoOmZkOWNoeWNs+WPr+mYheivuyIKICB9LAogICJmcmVlQm9va0lkcyI6IFtdLAogICJ0aW1lc3RhbXAiOiAxNjAzOTQwMjAzLAogICJyYW5kb20iOiA4MzQxLAogICJzaWduYXR1cmUiOiAiNWQ3ZjZjOTI5MDg2YTRhOTU4ZWFkNjBjOWJkOTVmYWU2OTkxYWZlZjUxYzFkZTUwYTQxZTdjMjY0MWZkYWYzZiIKfQ==';
 let body = $response.body
-wxreadData = new Base64().decode(wxreadData)
 body = JSON.stringify(wxreadData)
 $done({ body })
+
